78547e30230d534430d3af9947b1b8b6519bed12
IhyanM <116536123+IhyanM@users.noreply.github.com>
2023-02-26 13:20:25 +0700

    edit mitra

--- edit.blade.php
+++ edit.blade.php
@@ -1,6 +1,5 @@
 @extends('layout.master')
 
-
 @section('content')
 <section class="content">
     <div class="row">
@@ -8,27 +7,62 @@
             <div class="box">
                 <div class="box-header">
                     <div class="card-body">
-                        <form action="{{ route('jurusan.update', $jurusan->id_jurusan) }}" method="POST">
+                        <form action="{{ route('mitra.update', $mitra->id_mitra) }}" method="POST">
                             @csrf
                             @method('PUT')
 
                             <div class="form-group">
-                                <label for="nama_jurusan">Nama Jurusan</label>
-                                <input type="text" class="form-control @error('nama_jurusan') is-invalid @enderror" id="nama_jurusan" name="nama_jurusan" value="{{ $jurusan->nama_jurusan }}">
-                                @error('nama_jurusan')
+                                <label for="nama_mitra">Nama Mitra</label>
+                                <input type="text" class="form-control @error('nama_mitra') is-invalid @enderror" id="nama_mitra" name="nama_mitra" value="{{ $mitra->nama_mitra }}">
+                                @error('nama_mitra')
+                                    <span class="invalid-feedback">{{ $message }}</span>
+                                @enderror
+                            </div>
+
+                            <div class="form-group">
+                                <label for="alamat_mitra">Alamat Mitra</label>
+                                <input class="form-control @error('alamat_mitra') is-invalid @enderror" id="alamat_mitra" name="alamat_mitra" value="{{ $mitra->alamat_mitra }}">
+                                @error('alamat_mitra')
+                                    <span class="invalid-feedback">{{ $message }}</span>
+                                @enderror
+                            </div>
+
+                            <div class="form-group">
+                                <label for="telepon">Telepon</label>
+                                <input class="form-control @error('telepon') is-invalid @enderror" id="telepon" name="telepon" value="{{ $mitra->telepon }}">
+                                @error('telepon')
+                                    <span class="invalid-feedback">{{ $message }}</span>
+                                @enderror
+                            </div>
+
+                            <div class="form-group">
+                                <label for="email">Email</label>
+                                <input class="form-control @error('email') is-invalid @enderror" id="email" name="email" value="{{ $mitra->email }}">
+                                @error('email')
                                     <span class="invalid-feedback">{{ $message }}</span>
                                 @enderror
                             </div>
 
                             <div class="form-group">
                                 <label for="bidang">Bidang</label>
-                                <input class="form-control @error('bidang') is-invalid @enderror" id="bidang" name="bidang" value="{{ $jurusan->bidang }}">
-                                @error('bidang')
+                                <select name="bidang" id="bidang" class="form-control">
+                                    <option value="{{ $mitra->bidang }}">== Edit Bidang ==</option>
+                                    @foreach ($jurusan as $j)
+                                        <option value="{{ $j->bidang }}">{{ $j->bidang }}</option>
+                                    @endforeach
+                                </select>
+                            </div>
+
+                            <div class="form-group">
+                                <label for="keterangan">Keterangan</label>
+                                <input class="form-control @error('keterangan') is-invalid @enderror" id="keterangan" name="keterangan" value="{{ $mitra->keterangan }}">
+                                @error('keterangan')
                                     <span class="invalid-feedback">{{ $message }}</span>
                                 @enderror
                             </div>
 
-                            <button type="submit" class="btn btn-primary">Simpan</button>
+                            <button type="submit" class="btn btn-primary pull-right">Simpan</button>
+                            <a href="/mitra" class="btn btn-default pul-left">Kembali</a>
                         </form>
                     </div>
                 </div>
